a7c08de9c932e27b651d5c0b24a896510e8cecd4
Family <albon.136546120855@2depedqc.ph>
2023-06-05 19:09:18 +0800

    Function is added

--- calculator_app_inheritance.py
+++ calculator_app_inheritance.py
@@ -3,3 +3,6 @@ from SimpleAppCalculator import calculator_app
 # create the new class
 class NewFunction(calculator_app):
 # implement the new function
+    def exponent(self, var1, var2):
+        exponent = var1 ** var2
+        return exponent
