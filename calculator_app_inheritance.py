2f9e560bc0596bddd5abc83dac09fe2bbb93eebf
Family <albon.136546120855@2depedqc.ph>
2023-06-05 19:08:55 +0800

    Class is created

--- calculator_app_inheritance.py
+++ calculator_app_inheritance.py
@@ -1,4 +1,5 @@
 # import the existed python program for the reference of new class
 from SimpleAppCalculator import calculator_app
 # create the new class
+class NewFunction(calculator_app):
 # implement the new function
