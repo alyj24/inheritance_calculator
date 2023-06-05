4cf95131c42d6b97673820d18750e9ee2872c6d8
Family <albon.136546120855@2depedqc.ph>
2023-06-05 19:04:07 +0800

    Pseudocode is added for clarification

---
+++ calculator_app_inheritance.py
@@ -0,0 +1,7 @@
+from SimpleAppCalculator import calculator_app
+
+class AddedFunction(calculator_program):
+
+    def power_exponent(self, first_number, second_number):
+        power = first_number ** second_number
+        return power
--- user_inheritance_module.py
+++ user_inheritance_module.py
@@ -1,7 +1,8 @@
+# import the existed file for the reference of new module
 from UserInterface import user_module
 
-
+# create the class
 class UserInheritance(user_module):
-
+# implement the new method
     def exponent(self, exponent):
         print("\033[95mThe power of your input is: ", exponent")
